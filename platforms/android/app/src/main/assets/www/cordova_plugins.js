460c95297b71ae7471d9a5624673220284565517
alamnaryab <alamnaryab@ymail.com>
2020-01-31 01:03:00 +0400

    first commit

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-firebase-lib.FirebasePlugin",
+      "file": "plugins/cordova-plugin-firebase-lib/www/firebase.js",
+      "pluginId": "cordova-plugin-firebase-lib",
+      "clobbers": [
+        "FirebasePlugin"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-firebase-lib": "5.1.1",
+    "cordova-plugin-androidx": "1.0.2",
+    "cordova-plugin-androidx-adapter": "1.1.0"
+  };
+});
